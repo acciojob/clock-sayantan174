df81538eabdd7d815228efd03041180c7ab0a604
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-14 10:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const p = document.getElementById("timer");
+
+function getTime() {
+  const date = new Date();
+  let dayString = `${
+    date.getMonth() + 1
+  }/${date.getDate()}/${date.getFullYear()},`;
+  let hr = date.getHours();
+  let timeString = ``;
+  if (hr == 12)
+    timeString = `${hr}:${date.getMinutes()}:${date.getSeconds()} PM`;
+  else if (hr > 12)
+    timeString = `${hr - 12}:${date.getMinutes()}:${date.getSeconds()} PM`;
+  else timeString = `${hr}:${date.getMinutes()}:${date.getSeconds()} AM`;
+  return dayString + " " + timeString;
+}
+
+setInterval(() => {
+  p.innerText = getTime();
+}, 1000);
